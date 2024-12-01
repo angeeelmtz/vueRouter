0977a39e6b97756094e24fc6361335acabad5535
angeeelmtz <angelmarti18@icloud.com>
2024-11-30 22:12:11 -0500

    Concepts fundamentals about router vue

---
+++ src/views/BlogView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="blog">
+    <h1>This is an blog page</h1>
+    <ul>
+      <li><router-link :to="{name: 'post', params: {id:1}}">Primer post</router-link></li>
+      <li><router-link :to="{name: 'post', params: {id:2}}">Segundo post</router-link></li>
+      <li><router-link :to="{name: 'post', params: {id:3}}">Tercer post</router-link></li>
+      <li><router-link :to="{name: 'post', params: {id:4}}">Cuarto post</router-link></li>
+    </ul>
+  </div>
+
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+  ul{
+    list-style: none;
+  }
+</style>
---
+++ src/views/PostView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="post">
+        <h1>Post view del elemento : {{ postId }}</h1>
+    </div>
+</template>
+
+<script setup>
+    import { onMounted, ref } from 'vue'
+    import { useRoute } from 'vue-router'
+
+    let postId = ref('')
+
+    onMounted( () => {
+        const route = useRoute()
+        postId.value = route.params.id
+    })
+
+</script>
+
+<style scoped>
+</style>
